f5b1cf21ce65f0752032a72cdd916a6a72acb6e1
ChoHyeonSeok <hs.cho9647@gmail.com>
2022-04-30 14:52:29 +0900

    Add ButtonSet Toggle
    ButtonSet toggle
    add Info Detail Button

--- TowerDefenceVR/Assets/Scripts/ButtonEvents.cs
+++ TowerDefenceVR/Assets/Scripts/ButtonEvents.cs
@@ -2,9 +2,20 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class ButtonEvents : MonoBehaviour
 {
+    [SerializeField] private Text detail;
+    [SerializeField] private GameObject ButtonSet;
+    [SerializeField] private GameObject Label;
+    private bool isActivate = true;
+
+    private void Start() {
+        isActivate = false;
+        ButtonSet.SetActive(false);
+    }
+
     // Start is called before the first frame update
     public void gameStart()
     {
@@ -14,5 +25,26 @@ public class ButtonEvents : MonoBehaviour
     public void onClickExit()
     {
         Application.Quit();
+    }
+
+    public void onInfo() 
+    {
+        detail.text = "made by Team BigBrother";
+    }
+
+    public void onToggleButtonSet() 
+    {
+        if (isActivate) 
+        {
+            ButtonSet.SetActive(false);
+            Label.GetComponent<TextMesh>().text = "Off";
+            isActivate = !isActivate;
+        }
+        else 
+        {
+            ButtonSet.SetActive(true);
+            Label.GetComponent<TextMesh>().text = "On";
+            isActivate = !isActivate;
+        }
     }
 }
