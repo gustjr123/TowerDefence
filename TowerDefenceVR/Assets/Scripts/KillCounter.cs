a4c687616a52038a434dbe1d33f7091019b59402
ChoHyeonSeok <hs.cho9647@gmail.com>
2022-05-21 21:45:55 +0900

    Update killcounter
    Update killcounter

--- TowerDefenceVR/Assets/Scripts/KillCounter.cs
+++ TowerDefenceVR/Assets/Scripts/KillCounter.cs
@@ -33,6 +33,7 @@ public class KillCounter : MonoBehaviour
 
     [Header("ShieldEvent")]
     [SerializeField] private GameObject ShieldEvent;
+    private TimeManager TimeManager = new TimeManager();
 
     private bool IsEnd = false;
 
@@ -46,147 +47,92 @@ public class KillCounter : MonoBehaviour
         
     }
 
+    private bool ImageCheckPerPaze() {
+        if (isRight) {
+            if (nowCurrentHand == RightHandData) {
+                PauseManager.GetComponent<Pause>().NonPause();
+                nowPaze++;
+                ImageManager.GetComponent<ImageManager>().ImageSSuccess();
+                Debug.Log("nowPaze : " + nowPaze.ToString());
+                return true;
+            }
+        }
+        else {
+            if (nowCurrentHand == LeftHandData) {
+                PauseManager.GetComponent<Pause>().NonPause();
+                nowPaze++;
+                ImageManager.GetComponent<ImageManager>().ImageSSuccess();
+                Debug.Log("nowPaze : " + nowPaze.ToString());  
+                return true;              
+            }
+        }
+        return false;
+    }
+
+    private void ImageEventStart() {
+        PauseManager.GetComponent<Pause>().PauseGame();
+        readyImage = false;
+        PazeFunc();
+        IsPaze[nowPaze] = !IsPaze[nowPaze];
+    }
+
     // Update is called once per frame
     void Update()
     {
         ShowKills();
-        
-        if(RightHandData == Hands.Four) {
-            Debug.Log("Four OK");
+        // 킬수로 하나, 시간으로 하나 조건을 맞춰주면 됨
+        if (TimeManager.Minute >= 5 && nowPaze == 0) {
+        // if (kills >= 5 && nowPaze == 0) {
+            if (!IsPaze[nowPaze]) {
+                ImageEventStart();
+            }
+            // readyImage is Handled by ImageManager. It's set to true when ImageManager completely created ImagePrefab.
+            else if (readyImage) {
+                if (ImageCheckPerPaze()) {
+                    ShieldEvent.GetComponent<ShieldEvent>().OnShield1();
+                }
+            }
         }
-        if(LeftHandData == Hands.Four) {
-            Debug.Log("LFour OK");
+        if (TimeManager.Minute >= 10 && nowPaze == 1) {
+        // if (kills >= 10 && nowPaze == 1) {
+            if (!IsPaze[nowPaze]) {
+                ImageEventStart();
+            }
+            else if (readyImage) {
+                if (ImageCheckPerPaze()) {
+                    ShieldEvent.GetComponent<ShieldEvent>().OnShield2();
+                }
+            }
         }
-        if (kills >= 10)
-        {
-            if (nowPaze == 0) {
-                PauseManager.GetComponent<Pause>().PauseGame();
-                nowPaze = 1;
-                readyImage = false;
-                PazeFunc();
-                Debug.Log("nowPaze : " + nowPaze.ToString());
+        if (TimeManager.Minute >= 15 && nowPaze == 2) {
+        // if (kills >= 15 && nowPaze == 2) {
+            if (!IsPaze[nowPaze]) {
+                ImageEventStart();
             }
-            
-            //if (RightHandData == Hands.Peace)
-            // first
-            if (nowPaze == 1 && !IsPaze[nowPaze - 1] && readyImage)
-            {
-                if (isRight) {
-                    if (nowCurrentHand == RightHandData) {
-                        IsPaze[nowPaze - 1] = true;
-                        nowPaze = 2;
-                        ImageManager.GetComponent<ImageManager>().ImageSSuccess();
-                        readyImage = false;
-                        PazeFunc();
-                        Debug.Log("nowPaze : " + nowPaze.ToString());
-                        ShieldEvent.GetComponent<ShieldEvent>().OnShield1();
-                    }
-                }
-                else {
-                    if (nowCurrentHand == LeftHandData) {
-                        IsPaze[nowPaze - 1] = true;
-                        nowPaze = 2;
-                        ImageManager.GetComponent<ImageManager>().ImageSSuccess();
-                        readyImage = false;
-                        PazeFunc();
-                        Debug.Log("nowPaze : " + nowPaze.ToString());
-                        ShieldEvent.GetComponent<ShieldEvent>().OnShield1();
-                    }
+            else if (readyImage) {
+                if (ImageCheckPerPaze()) {
+                    ShieldEvent.GetComponent<ShieldEvent>().OnShield3();
                 }
             }
-            // second
-            if (nowPaze == 2 && !IsPaze[nowPaze - 1] && readyImage)
-            {
-                if (isRight) {
-                    if (nowCurrentHand == RightHandData) {
-                        IsPaze[nowPaze - 1] = true;
-                        nowPaze = 3;
-                        ImageManager.GetComponent<ImageManager>().ImageSSuccess();
-                        readyImage = false;
-                        PazeFunc();
-                        Debug.Log("nowPaze : " + nowPaze.ToString());
-                        ShieldEvent.GetComponent<ShieldEvent>().OnShield2();
-                    }
-                }
-                else {
-                    if (nowCurrentHand == LeftHandData) {
-                        IsPaze[nowPaze - 1] = true;
-                        nowPaze = 3;
-                        ImageManager.GetComponent<ImageManager>().ImageSSuccess();
-                        readyImage = false;
-                        PazeFunc();
-                        Debug.Log("nowPaze : " + nowPaze.ToString());
-                        ShieldEvent.GetComponent<ShieldEvent>().OnShield2();
-                    }
+        }
+        if (TimeManager.Minute >= 20 && nowPaze == 3) {
+        // if (kills >= 20 && nowPaze == 3) {
+            if (!IsPaze[nowPaze]) {
+                ImageEventStart();
+            }
+            else if (readyImage) {
+                if (ImageCheckPerPaze()) {
+                    ShieldEvent.GetComponent<ShieldEvent>().OnShield4();
+                    ShieldEvent.GetComponent<ShieldEvent>().OnShieldTower();
                 }
             }
-            // third
-            if (nowPaze == 3 && !IsPaze[nowPaze - 1] && readyImage)
-            {
-                if (isRight) {
-                    if (nowCurrentHand == RightHandData) {
-                        IsPaze[nowPaze - 1] = true;
-                        nowPaze = 4;
-                        ImageManager.GetComponent<ImageManager>().ImageSSuccess();
-                        readyImage = false;
-                        PazeFunc();
-                        Debug.Log("nowPaze : " + nowPaze.ToString());
-                        ShieldEvent.GetComponent<ShieldEvent>().OnShield3();
-                    }
-                }
-                else {
-                    if (nowCurrentHand == LeftHandData) {
-                        IsPaze[nowPaze - 1] = true;
-                        nowPaze = 4;
-                        ImageManager.GetComponent<ImageManager>().ImageSSuccess();
-                        readyImage = false;
-                        PazeFunc();
-                        Debug.Log("nowPaze : " + nowPaze.ToString());
-                        ShieldEvent.GetComponent<ShieldEvent>().OnShield3();
-                    }
-                }
-            }
-            // fourth
-            if (nowPaze == 4 && !IsPaze[nowPaze - 1] && readyImage)
-            {
-                if (isRight) {
-                    if (nowCurrentHand == RightHandData) {
-                        IsPaze[nowPaze - 1] = true;
-                        nowPaze = 5;
-                        ImageManager.GetComponent<ImageManager>().ImageSSuccess();
-                        readyImage = false;
-                        Debug.Log("nowPaze : " + nowPaze.ToString());
-                        ShieldEvent.GetComponent<ShieldEvent>().OnShield4();
-                        ShieldEvent.GetComponent<ShieldEvent>().OnShieldTower();
-                        IsEnd = true;
-                    }
-                }
-                else {
-                    if (nowCurrentHand == LeftHandData) {
-                        IsPaze[nowPaze - 1] = true;
-                        nowPaze = 5;
-                        ImageManager.GetComponent<ImageManager>().ImageSSuccess();
-                        readyImage = false;
-                        Debug.Log("nowPaze : " + nowPaze.ToString());
-                        ShieldEvent.GetComponent<ShieldEvent>().OnShield4();
-                        ShieldEvent.GetComponent<ShieldEvent>().OnShieldTower();
-                        IsEnd = true;
-                    }
-                }
-            }
-
-            // Last Paze
-            if (nowPaze >= 5 && IsEnd)
-            {
-                Debug.Log("Last Paze");
-                PauseManager.GetComponent<Pause>().NonPause();
-                StartCoroutine(FinalEndingPaze());
-                // DataReset();
-                // ImageManager.GetComponent<ImageManager>().Reset();
-                // // GameClear and Clear Impact open
-                
-                // SceneManager.LoadScene("resultScene");
-            }
+        }
+        // ending
+        if (TimeManager.Minute >= 20 && nowPaze == 4) {
+        // if (kills >= 20 && nowPaze == 4) {
+            Debug.Log("Last Paze");
+            PauseManager.GetComponent<Pause>().NonPause();
+            StartCoroutine(FinalEndingPaze());
         }
     }
 
@@ -256,7 +202,7 @@ public class KillCounter : MonoBehaviour
     private IEnumerator FinalEndingPaze()
     {
         IsEnd = false;
-        yield return new WaitForSeconds(10f);
+        yield return new WaitForSeconds(2f);
         DataReset();
         ImageManager.GetComponent<ImageManager>().Reset();
         // GameClear and Clear Impact open
