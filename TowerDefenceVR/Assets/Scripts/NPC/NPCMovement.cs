ffce678551d56e22d9491b6a6361889c4dd54f1f
smk903 <sbj428@naver.com>
2022-05-26 19:15:38 +0900

    NPC 이동, 패널 시간 조정

--- TowerDefenceVR/Assets/Scripts/NPC/NPCMovement.cs
+++ TowerDefenceVR/Assets/Scripts/NPC/NPCMovement.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class NPCMovement : MonoBehaviour
 {
@@ -9,8 +10,8 @@ public class NPCMovement : MonoBehaviour
     private int targetWaypointIndex;
     private float minDistance = 0.1f;
     private int lastWaypointIndex;
-
     private float movementSpeed = 1.0f;
+    float timer;
 
     void Start()
     {
@@ -19,12 +20,21 @@ public class NPCMovement : MonoBehaviour
 
     void Update()
     {
-        //float movementStep = movementSpeed * Time.deltaTime;
+        timer += Time.deltaTime;
         Vector3 target = new Vector3(2.5f, 0f, -14f);
+        Vector3 target2 = new Vector3(0, 0, -10f);
         //float distance = Vector3.Distance(transform.position, targetWaypoint.position);
         //CheckDistanceToWaypoint(distance);
 
-        transform.position = Vector3.MoveTowards(transform.position, target, 1f);
+        if (timer > 2.95)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, target, 0.1f);
+        }
+
+        if (timer > 8)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, target2, 0.125f);
+        }
     }
     /*
     void CheckDistanceToWaypoint(float currentDistance)
--- TowerDefenceVR/Assets/Scripts/NPC/PanelScript/Panel.cs
+++ TowerDefenceVR/Assets/Scripts/NPC/PanelScript/Panel.cs
@@ -5,6 +5,9 @@ using UnityEngine.UI;
 
 public class Panel : MonoBehaviour
 {
+    public GameObject Panel0;
+    public GameObject Panel01;
+
     public GameObject Panel1;
     public GameObject Panel2;
     public GameObject Panel3;
@@ -24,7 +27,23 @@ public class Panel : MonoBehaviour
     {
         currTime += Time.deltaTime;
 
-        if (currTime > 4.95 && currTime < 5)
+        if (currTime > 0 && currTime < 0.05)
+        {
+            Vector3 v = new Vector3(SlimeGuide.transform.position.x, SlimeGuide.transform.position.y + 2, SlimeGuide.transform.position.z);
+            GameObject Dialog0 = Instantiate(Panel0, v, Quaternion.identity);
+            Panel0.SetActive(true);
+            Destroy(Dialog0, 0.2f);
+        }
+
+        if (currTime > 1.95 && currTime < 2)
+        {
+            Vector3 v = new Vector3(SlimeGuide.transform.position.x, SlimeGuide.transform.position.y + 2, SlimeGuide.transform.position.z);
+            GameObject Dialog01 = Instantiate(Panel01, v, Quaternion.identity);
+            Panel0.SetActive(true);
+            Destroy(Dialog01, 0.2f);
+        }
+
+        if (currTime > 14.95 && currTime < 15)
         {
             Vector3 v = new Vector3(SlimeGuide.transform.position.x, SlimeGuide.transform.position.y+2, SlimeGuide.transform.position.z);
             GameObject Dialog = Instantiate(Panel1, v, Quaternion.identity);
@@ -33,7 +52,7 @@ public class Panel : MonoBehaviour
             Destroy(Dialog, 0.2f);
         }
 
-        if (currTime > 9.95 && currTime < 10)
+        if (currTime > 29.95 && currTime < 30)
         {
             Vector3 v = new Vector3(SlimeGuide.transform.position.x, SlimeGuide.transform.position.y+2, SlimeGuide.transform.position.z);
             GameObject Dialog2 = Instantiate(Panel2, v, Quaternion.identity);
@@ -42,7 +61,7 @@ public class Panel : MonoBehaviour
             Destroy(Dialog2, 0.2f);
         }
 
-        if (currTime > 14.95 && currTime < 15)
+        if (currTime > 44.95 && currTime < 45)
         {
             Vector3 v = new Vector3(SlimeGuide.transform.position.x, SlimeGuide.transform.position.y+2, SlimeGuide.transform.position.z);
             GameObject Dialog3 = Instantiate(Panel3, v, Quaternion.identity);
@@ -51,7 +70,7 @@ public class Panel : MonoBehaviour
             Destroy(Dialog3, 0.2f);
         }
 
-        if (currTime > 19.95 && currTime <20)
+        if (currTime > 59.95 && currTime <60)
         {
             Vector3 v = new Vector3(SlimeGuide.transform.position.x, SlimeGuide.transform.position.y+2, SlimeGuide.transform.position.z);
             GameObject Dialog4 = Instantiate(Panel4, v, Quaternion.identity);
@@ -60,7 +79,7 @@ public class Panel : MonoBehaviour
             Destroy(Dialog4, 0.2f);
         }
 
-        if (currTime > 24.95 && currTime < 25)
+        if (currTime > 62.95)
         {
             Vector3 v = new Vector3(SlimeGuide.transform.position.x, SlimeGuide.transform.position.y+2, SlimeGuide.transform.position.z);
             GameObject Dialog5 = Instantiate(Panel5, v, Quaternion.identity);
